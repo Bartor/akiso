93457c531334d159d6571d5c75b7729271dfe8fe
Bartosz Rajczyk <bartorkrajczyk@gmail.com>
2018-11-29 19:05:53 +0100

    lepszy sposób na sprawdzanie

--- 4/2-1.c
+++ 4/2-1.c
@@ -3,14 +3,13 @@
 #include <unistd.h>
 
 void sig_handler(int signo) {
-	printf("received\n");
+	printf("received %d\n", signo);
 }
 
 int main() {
-	if(signal(SIGINT, sig_handler) == SIG_ERR) printf("cannot catch SIGINT\n");
-	if(signal(SIGKILL, sig_handler) == SIG_ERR) printf("cannot catch SIGKILL\n");
-	if(signal(SIGSTOP, sig_handler) == SIG_ERR) printf("cannot catch SIGSTOP\n");
-	if(signal(SIGUSR1, sig_handler) == SIG_ERR) printf("cannot catch SIGUSR1\n");
+	for (int i = 1; i <= 64; i++) {
+		if (signal(i, sig_handler) == SIG_ERR) printf("cannot catch %d\n", i);
+	}
 	while(1) {
 		printf("working\n");
 		sleep(1);
