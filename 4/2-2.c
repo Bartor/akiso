42e98e8667859b5f8eb239357f3debbd019f64e8
Bartosz Rajczyk <bartorkrajczyk@gmail.com>
2018-11-30 11:11:09 +0100

    poprawka w 2-2

--- 4/2-2.c
+++ 4/2-2.c
@@ -4,6 +4,6 @@
 
 int main() {
 	setuid(0);
-	printf("%d\n", kill(-1, SIGKILL)); //nie da się tego zrobić, bo man 2 kill
+	printf("%d\n", kill(1, SIGKILL)); //nie da się tego zrobić, bo man 2 kill
 	return 0;
 }
